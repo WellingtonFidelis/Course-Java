a976c6dff1869a5bf20cc360da098ac8126d9cdd
WellingtonFidelis <wellingtonf20@gmail.com>
2019-12-19 13:10:07 -0300

    Last save of exercise number 3 from classes 25 to 27

--- src/aula25a27_Exercicio3/Aluno.java
+++ src/aula25a27_Exercicio3/Aluno.java
@@ -3,7 +3,39 @@ package aula25a27_Exercicio3;
 public class Aluno {
 	String name;
 	String matricula;
-	String Curso;
-	String[] disciplina;
-	double[][] notas;
+	String curso;
+	String[] disciplina = new String [3];
+	double[][] notas = new double [3][4];
+	
+	void mostrarInfoAluno () {
+		System.out.println("Nome: " + name);
+		System.out.println("Matricula: " + matricula);
+		System.out.println("Nome do curso: " + curso);
+		
+		for (int i=0; i< notas.length; i++) {
+			System.out.println("Notas da disciplina " + disciplina[i]);
+			for (int j=0; j< notas[i].length; j++) {
+				System.out.print(notas[i][j] + "   ");
+			}  
+			System.out.println();
+		}
+	}
+	
+	public boolean verificarAprovacao(int indiceDisciplina) {
+		
+		double soma = 0;
+		
+		for (int i=0 ; i < notas[indiceDisciplina].length; i++) {
+			soma += notas[indiceDisciplina][i];
+		}
+		
+		double media = soma / 4;
+		
+		if (media >= 7) {
+			return true;
+		}
+		
+		return false; 
+		
+	}
 }
--- src/aula25a27_Exercicio3/AppTesteAluno.java
+++ src/aula25a27_Exercicio3/AppTesteAluno.java
@@ -9,8 +9,41 @@ public class AppTesteAluno {
 	public static void main(String[] args) {
 	
 		// input data
+		Aluno aluno = new Aluno();
+		
 		System.out.println("Digite o nome do aluno:");
-		Aluno.nome = scan.nextLine();
+		aluno.name = scan.nextLine();
+		
+		System.out.println("Digite a matricula do aluno:");
+		aluno.curso = scan.nextLine();
+		
+		System.out.println("Digite a matricula do aluno:");
+		aluno.matricula = scan.nextLine();
+		
+		// aluno.disciplina = new String[3];
+		for (int i=0; i<aluno.disciplina.length; i++) {
+			System.out.println("Digite o nome da " + (i+1) + " disciplina:");
+			aluno.disciplina[i] = scan.nextLine();
+		}
+		
+		for (int i=0; i < aluno.notas.length; i++) {
+			System.out.println("obtendo as notas da disciplina" + aluno.disciplina[i]);
+			
+			for (int j=0; j < aluno.notas[i].length; j++) {
+				System.out.println("digite a nota " + (j+1));
+				aluno.notas[i][j] = scan.nextDouble();
+			}
+		}
+		
+		aluno.mostrarInfoAluno();
+		
+		for (int i=0; i < aluno.disciplina.length; i++) {
+			if (aluno.verificarAprovacao(i)) {
+				System.out.println("Disciplina " + aluno.disciplina[i] + " --> foi Aprovado!");
+			} else {
+				System.out.println("Disciplina " + aluno.disciplina[i] + " --> foi Reprovado");
+			}
+		}
 	}
 
 }
